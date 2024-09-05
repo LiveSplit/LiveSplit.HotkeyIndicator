0642a32068882756475ea37c17c78fd81c4449e6
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:18 +0200

    Add `.editorconfig` & apply style rules (#10)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.HotkeyIndicator/HotkeyIndicator.cs
+++ src/LiveSplit.HotkeyIndicator/HotkeyIndicator.cs
@@ -1,117 +1,120 @@
-﻿using LiveSplit.Model;
-using LiveSplit.UI;
-using LiveSplit.UI.Components;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace LiveSplit
+using LiveSplit.Model;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+
+namespace LiveSplit;
+
+public class HotkeyIndicator : IComponent
 {
-    public class HotkeyIndicator : IComponent
+    public float PaddingTop => 0f;
+    public float PaddingLeft => 0f;
+    public float PaddingBottom => 0f;
+    public float PaddingRight => 0f;
+
+    protected LineComponent Line { get; set; }
+
+    public Color CurrentColor { get; set; }
+
+    public HotkeyIndicatorSettings Settings { get; set; }
+
+    public GraphicsCache Cache { get; set; }
+
+    public float VerticalHeight => Settings.IndicatorHeight;
+
+    public float MinimumWidth => 0f;
+
+    public float HorizontalWidth => Settings.IndicatorWidth;
+
+    public float MinimumHeight => 0f;
+
+    public HotkeyIndicator()
     {
-        public float PaddingTop => 0f;
-        public float PaddingLeft => 0f;
-        public float PaddingBottom => 0f;
-        public float PaddingRight => 0f;
+        Line = new LineComponent(3, Color.White);
+        Cache = new GraphicsCache();
+        Settings = new HotkeyIndicatorSettings();
+    }
 
-        protected LineComponent Line { get; set; }
+    public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        Region oldClip = g.Clip;
+        System.Drawing.Drawing2D.Matrix oldMatrix = g.Transform;
+        System.Drawing.Drawing2D.SmoothingMode oldMode = g.SmoothingMode;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+        g.Clip = new Region();
+        Line.LineColor = CurrentColor;
+        float scale = g.Transform.Elements.First();
+        float newHeight = Math.Max((int)((Settings.IndicatorHeight * scale) + 0.5f), 1) / scale;
+        Line.VerticalHeight = newHeight;
+        g.TranslateTransform(0, (Settings.IndicatorHeight - newHeight) / 2f);
+        Line.DrawVertical(g, state, width, clipRegion);
+        g.Clip = oldClip;
+        g.Transform = oldMatrix;
+        g.SmoothingMode = oldMode;
+    }
 
-        public Color CurrentColor { get; set; }
+    public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        Region oldClip = g.Clip;
+        System.Drawing.Drawing2D.Matrix oldMatrix = g.Transform;
+        System.Drawing.Drawing2D.SmoothingMode oldMode = g.SmoothingMode;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+        g.Clip = new Region();
+        Line.LineColor = CurrentColor;
+        float scale = g.Transform.Elements.First();
+        float newWidth = Math.Max((int)((Settings.IndicatorWidth * scale) + 0.5f), 1) / scale;
+        g.TranslateTransform((Settings.IndicatorWidth - newWidth) / 2f, 0);
+        Line.HorizontalWidth = newWidth;
+        Line.DrawHorizontal(g, state, height, clipRegion);
+        g.Clip = oldClip;
+        g.Transform = oldMatrix;
+        g.SmoothingMode = oldMode;
+    }
 
-        public HotkeyIndicatorSettings Settings { get; set; }
+    public string ComponentName => "Hotkey Indicator";
 
-        public GraphicsCache Cache { get; set; }
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        Settings.Mode = mode;
+        return Settings;
+    }
 
-        public float VerticalHeight => Settings.IndicatorHeight;
+    public void SetSettings(System.Xml.XmlNode settings)
+    {
+        Settings.SetSettings(settings);
+    }
 
-        public float MinimumWidth => 0f;
+    public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+    {
+        return Settings.GetSettings(document);
+    }
 
-        public float HorizontalWidth => Settings.IndicatorWidth;
+    public IDictionary<string, Action> ContextMenuControls => null;
 
-        public float MinimumHeight => 0f;
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        CurrentColor = state.Settings.HotkeyProfiles[state.CurrentHotkeyProfile].GlobalHotkeysEnabled ? Settings.HotkeysOnColor : Settings.HotkeysOffColor;
 
-        public HotkeyIndicator()
+        Cache.Restart();
+        Cache["IndicatorColor"] = CurrentColor.ToArgb();
+
+        if (invalidator != null && Cache.HasChanged)
         {
-            Line = new LineComponent(3, Color.White);
-            Cache = new GraphicsCache();
-            Settings = new HotkeyIndicatorSettings();
+            invalidator.Invalidate(0, 0, width, height);
         }
+    }
 
-        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
-        {
-            var oldClip = g.Clip;
-            var oldMatrix = g.Transform;
-            var oldMode = g.SmoothingMode;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
-            g.Clip = new Region();
-            Line.LineColor = CurrentColor;
-            var scale = g.Transform.Elements.First();
-            var newHeight = Math.Max((int)(Settings.IndicatorHeight * scale + 0.5f), 1) / scale;
-            Line.VerticalHeight = newHeight;
-            g.TranslateTransform(0, (Settings.IndicatorHeight - newHeight) / 2f);
-            Line.DrawVertical(g, state, width, clipRegion);
-            g.Clip = oldClip;
-            g.Transform = oldMatrix;
-            g.SmoothingMode = oldMode;
-        }
+    public void Dispose()
+    {
+    }
 
-        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
-        {
-            var oldClip = g.Clip;
-            var oldMatrix = g.Transform;
-            var oldMode = g.SmoothingMode;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
-            g.Clip = new Region();
-            Line.LineColor = CurrentColor;
-            var scale = g.Transform.Elements.First();
-            var newWidth = Math.Max((int)(Settings.IndicatorWidth * scale + 0.5f), 1) / scale;
-            g.TranslateTransform((Settings.IndicatorWidth - newWidth) / 2f, 0);
-            Line.HorizontalWidth = newWidth;
-            Line.DrawHorizontal(g, state, height, clipRegion);
-            g.Clip = oldClip;
-            g.Transform = oldMatrix;
-            g.SmoothingMode = oldMode;
-        }
-
-        public string ComponentName => "Hotkey Indicator";
-
-        public Control GetSettingsControl(LayoutMode mode)
-        {
-            Settings.Mode = mode;
-            return Settings;
-        }
-
-        public void SetSettings(System.Xml.XmlNode settings)
-        {
-            Settings.SetSettings(settings);
-        }
-
-
-        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
-        {
-            return Settings.GetSettings(document);
-        }
-
-        
-        public IDictionary<string, Action> ContextMenuControls => null;
-
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-        {
-            CurrentColor = state.Settings.HotkeyProfiles[state.CurrentHotkeyProfile].GlobalHotkeysEnabled ? Settings.HotkeysOnColor : Settings.HotkeysOffColor;
-
-            Cache.Restart();
-            Cache["IndicatorColor"] = CurrentColor.ToArgb();
-
-            if (invalidator != null && Cache.HasChanged)
-                invalidator.Invalidate(0, 0, width, height);
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+    public int GetSettingsHashCode()
+    {
+        return Settings.GetSettingsHashCode();
     }
 }
